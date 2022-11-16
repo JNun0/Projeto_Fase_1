d447e7651e03eb674e89e7c7ad3ef4086d0cda2f
JNun0 <joao.nuno669@outlook.pt>
2022-11-16 19:26:59 +0000

    Update
    Adição do movimento com as cores

--- main_cv.py
+++ main_cv.py
@@ -22,10 +22,9 @@ def cv_update(game):
     image = image[:, ::-1, :]
     image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    verde_min = np.array([25, 52, 72])
-    verde_max = np.array([102, 255, 255])
+    verde_min = np.array([34, 42, 0])
+    verde_max = np.array([84, 255, 255])
     verde_mascara = cv2.inRange(image_hsv, verde_min, verde_max)
-    # mete a mascára por cima da image para apenas aparecer a cor
     verde = cv2.bitwise_and(image, image, mask=verde_mascara)
 
     vermelho_min = np.array([161, 155, 84])
@@ -33,21 +32,32 @@ def cv_update(game):
     vermelho_mascara = cv2.inRange(image_hsv, vermelho_min, vermelho_max)
     vermelho = cv2.bitwise_and(image, image, mask=vermelho_mascara)
 
-    cv_process2(verde)
+    cv_process(verde)
     cv_output2(verde)
+
+    cv_process(image)
+    cv_output3(image)
 
     cv_process(vermelho)
     cv_output(vermelho)
-    # game.paddle.move(-1)
+
+    vermelho_existe = np.sum(vermelho_mascara)
+    verde_existe = np.sum(verde_mascara)
+
+    if verde_existe > 200000:
+        game.paddle.move(+1)
+    else:
+        game.paddle.move(0)
+
+    if vermelho_existe > 200000:
+        game.paddle.move(-1)
+    else:
+        game.paddle.move(0)
+
     game.after(1, cv_update, game)
 
 
 def cv_process(image):
-    # main image processing code
-    pass
-
-
-def cv_process2(image):
     # main image processing code
     pass
 
@@ -60,5 +70,11 @@ def cv_output(image):
 
 def cv_output2(image):
     cv2.imshow("Verde", image)
+    # rest of output rendering
+    cv2.waitKey(1)
+
+
+def cv_output3(image):
+    cv2.imshow("Imagem", image)
     # rest of output rendering
     cv2.waitKey(1)
