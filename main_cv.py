d2255b67b4c682e3ce63aa0ecdb2f133d3aab48e
JNun0 <joao.nuno669@outlook.pt>
2022-11-17 20:54:05 +0000

    Update Código
    Organização

--- main_cv.py
+++ main_cv.py
@@ -19,62 +19,74 @@ def cv_update(game):
     if not cap.isOpened():
         cap.open(0)
     ret, image = cap.read()
-    image = image[:, ::-1, :]
-    image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    image = image[:, ::-1, :]   #Inverte Imagem
+    image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)  # Imagem BGR para HSV
 
-    verde_min = np.array([34, 42, 0])
-    verde_max = np.array([84, 255, 255])
-    verde_mascara = cv2.inRange(image_hsv, verde_min, verde_max)
-    verde = cv2.bitwise_and(image, image, mask=verde_mascara)
+    verde_min = np.array([34, 42, 50])  # verde escuro
+    verde_max = np.array([84, 255, 255])  # verde claro
+    verde_mascara = cv2.inRange(image_hsv, verde_min, verde_max)  # cores entre ambos
 
     vermelho_min = np.array([161, 155, 84])
-    vermelho_max = np.array([179, 255, 255])
+    vermelho_max = np.array([180, 255, 255])
     vermelho_mascara = cv2.inRange(image_hsv, vermelho_min, vermelho_max)
-    vermelho = cv2.bitwise_and(image, image, mask=vermelho_mascara)
 
-    cv_process(verde)
-    cv_output2(verde)
-
-    cv_process(image)
-    cv_output3(image)
-
-    cv_process(vermelho)
-    cv_output(vermelho)
+    cv_process_green(image, verde_mascara)
+    cv_process_red(image, vermelho_mascara)
+    cv_output(image)
 
     vermelho_existe = np.sum(vermelho_mascara)
     verde_existe = np.sum(verde_mascara)
 
-    if verde_existe > 200000:
-        game.paddle.move(+1)
+    if verde_existe > 500000: #número de pixeis verdes > 500 000
+        game.paddle.move(+3)
     else:
         game.paddle.move(0)
 
-    if vermelho_existe > 200000:
-        game.paddle.move(-1)
+    if vermelho_existe > 500000:
+        game.paddle.move(-3)
     else:
         game.paddle.move(0)
 
     game.after(1, cv_update, game)
 
 
-def cv_process(image):
-    # main image processing code
-    pass
+def cv_process_green(image, verde_mascara):
+
+    verde = cv2.bitwise_and(image, image, mask=verde_mascara)
+
+    kernel = np.ones((5, 5), np.uint8)
+
+    erosion_vr = cv2.erode(verde, kernel, iterations=1)
+    dilate_vr = cv2.dilate(erosion_vr, kernel, iterations=1)
+
+    cv_output_green(dilate_vr)
+
+
+def cv_process_red(image, vermelho_mascara):
+
+    vermelho = cv2.bitwise_and(image, image, mask=vermelho_mascara)
+
+    kernel = np.ones((5, 5), np.uint8)
+
+    erosion_vm = cv2.erode(vermelho, kernel, iterations=1)
+    dilate_vm = cv2.dilate(erosion_vm, kernel, iterations=1)
+
+    cv_output_red(dilate_vm)
 
 
 def cv_output(image):
+    cv2.imshow("Imagem", image)
+    # rest of output rendering
+    cv2.waitKey(1)
+
+
+def cv_output_red(image):
     cv2.imshow("Vermelho", image)
     # rest of output rendering
     cv2.waitKey(1)
 
 
-def cv_output2(image):
+def cv_output_green(image):
     cv2.imshow("Verde", image)
-    # rest of output rendering
-    cv2.waitKey(1)
-
-
-def cv_output3(image):
-    cv2.imshow("Imagem", image)
     # rest of output rendering
     cv2.waitKey(1)
